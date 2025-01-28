77b414a1863dafbec4a5882f1a402e7342243001
isomo <jiahaoxiang2000@gmail.com>
2025-01-28 16:23:44 +0800

    doc: throw the other info form the readmd.md

--- src/extension.ts
+++ src/extension.ts
@@ -19,7 +19,9 @@ export function activate(context: vscode.ExtensionContext) {
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from ai-coder!");
+      vscode.window.showInformationMessage(
+        "Hello World from ai-coder! this support by the isomo"
+      );
     }
   );
 
